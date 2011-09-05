393496d548242bde7b756b75fd811ec015e84ab8
jianhuashao <jianhua.shao1986@gmail.com>
2011-09-06 00:41:41 +0100

    init project in NetBean

---
+++ PhDTwitterEcosystem/src/phdtwitterecosystem.py
@@ -0,0 +1,8 @@
+# To change this template, choose Tools | Templates
+# and open the template in the editor.
+
+__author__="jianhuashao"
+__date__ ="$Sep 6, 2011 12:39:30 AM$"
+
+if __name__ == "__main__":
+    print "Hello World"
